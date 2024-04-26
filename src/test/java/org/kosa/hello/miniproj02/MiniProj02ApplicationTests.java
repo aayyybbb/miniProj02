c2f000a117299b0c37eecb9ffaa9cdb76fd9d8ae
anyoonbin <yb226@kakao.com>
2024-04-26 14:25:33 +0900

    프로젝트 생성

---
+++ src/test/java/org/kosa/hello/miniproj02/MiniProj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosa.hello.miniproj02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProj02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
